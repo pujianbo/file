4d2347e9b88899d81de03ca4b368e70c9409ef69
ChenMohan <719078369@qq.com>
2018-08-30 13:51:21 +0800

    新插件

--- test.js
+++ test.js
@@ -1,27 +1,2 @@
-var name='first name'
-function test(){
-  this.name='pujianbo';
-  this.say=function(){
-    setTimeout(()=>{
-      console.log(this.name);
-    },1000)
-  }
-}
-var t=new test()
-t.say()()
 
-this.name='zhags'
-function Course(){
-    this.name = "name";
-    this.description = "description";
-    this.author = "author";
-    this.getSummary = function(){
-        return this.name + ", " + this.description;
-    };
-    this.getDetails = function(){
-        window.setTimeout(() => {console.log(this.name + " " + this.author)},1000);
-    }
-}
-
-var course = new Course();
-course.getDetails();
+console.log(23 + 33);
